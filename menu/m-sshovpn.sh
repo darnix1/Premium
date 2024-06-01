6538bbe221b1078ba2962a45fd725a050c332dd6
darnix1 <146903037+darnix1@users.noreply.github.com>
2024-06-01 10:13:38 -0600

    Update m-sshovpn.sh

--- menu/m-sshovpn.sh
+++ menu/m-sshovpn.sh
@@ -1212,6 +1212,36 @@ read -n 1 -s -r -p "Press any key to back on menu"
 m-sshovpn
 }
 clear
+function liser () {
+clear
+
+
+echo -e "$siani╒════════════════════════════════════════════╕\033[0m"
+echo -e " \033[0;41;36m\033[1mUSERNAME          EXP DATE        STATUS   \033[0m"
+echo -e "$siani╘════════════════════════════════════════════╛\033[0m"
+
+unlocked_count=0
+
+while IFS=':' read -r user _ uid _; do
+    if [[ $uid -ge 1000 && $user != "nobody" ]]; then
+        exp=$(chage -l "$user" | grep "Account expires" | awk -F": " '{print $2}')
+        status=$(passwd -S "$user" | awk '{print $2}')
+        if [[ "$exp" != "never" ]]; then
+            if [[ "$status" = "L" ]]; then
+                printf "%-17s %2s %-17s %2s \n" "  $user" "$exp     " "LOCKED"
+            else
+                printf "%-17s %2s %-17s %2s \n" "  $user" "$exp     " "UNLOCKED"
+                ((unlocked_count++))
+            fi
+        fi
+    fi
+done < /etc/passwd
+
+echo -e "$siani╒═════════════════════════════════════════════╕\033[0m"
+echo "           TOTAL: $unlocked_count  User(s)"
+enter
+m-sshovpn
+}
 function listssh(){
 clear
 echo -e "$COLOR1┌──────────────────────────────────────────┐${NC}"
