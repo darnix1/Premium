d85683543615abaf0a6bd2335ddf66e6de914a4e
darnix1 <146903037+darnix1@users.noreply.github.com>
2025-04-03 12:28:03 -0600

    Update update.sh

--- menu/update.sh
+++ menu/update.sh
@@ -1,35 +1,67 @@
 #!/bin/bash
-dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
-biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
-###########- COLOR CODE -##############
-echo -e " [INFO] Downloading File"
-sleep 2
-wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/darnix1/Premium/main/menu/menu.sh" && chmod +x /usr/bin/menu
-wget -q -O /usr/bin/m-ip "https://raw.githubusercontent.com/darnix1/Premium/main/menu/m-ip.sh" && chmod +x /usr/bin/m-ip
-wget -q -O /usr/bin/m-bot "https://raw.githubusercontent.com/darnix1/Premium/main/menu/m-bot.sh" && chmod +x /usr/bin/m-bot
-wget -q -O /usr/bin/update "https://raw.githubusercontent.com/darnix1/Premium/main/menu/update.sh" && chmod +x /usr/bin/update
-wget -q -O /usr/bin/m-theme "https://raw.githubusercontent.com/darnix1/Premium/main/menu/m-theme.sh" && chmod +x /usr/bin/m-theme
-wget -q -O /usr/bin/m-vmess "https://raw.githubusercontent.com/darnix1/Premium/main/menu/m-vmess.sh" && chmod +x /usr/bin/m-vmess
-wget -q -O /usr/bin/m-vless "https://raw.githubusercontent.com/darnix1/Premium/main/menu/m-vless.sh" && chmod +x /usr/bin/m-vless
-wget -q -O /usr/bin/m-trojan "https://raw.githubusercontent.com/darnix1/Premium/main/menu/m-trojan.sh" && chmod +x /usr/bin/m-trojan
-wget -q -O /usr/bin/m-system "https://raw.githubusercontent.com/darnix1/Premium/main/menu/m-system.sh" && chmod +x /usr/bin/m-system
-wget -q -O /usr/bin/m-sshovpn "https://raw.githubusercontent.com/darnix1/Premium/main/menu/m-sshovpn.sh" && chmod +x /usr/bin/m-sshovpn
-wget -q -O /usr/bin/m-ssws "https://raw.githubusercontent.com/darnix1/Premium/main/menu/m-ssws.sh" && chmod +x /usr/bin/m-ssws
-wget -q -O /usr/bin/running "https://raw.githubusercontent.com/darnix1/Premium/main/menu/running.sh" && chmod +x /usr/bin/running
-wget -q -O /usr/bin/m-backup "https://raw.githubusercontent.com/darnix1/Premium/main/menu/m-backup.sh" && chmod +x /usr/bin/m-backup
-wget -q -O /usr/bin/m-update "https://raw.githubusercontent.com/darnix1/Premium/main/menu/m-update.sh" && chmod +x /usr/bin/m-update
-wget -q -O /usr/bin/speedtest "https://raw.githubusercontent.com/darnix1/Premium/main/speedtest_cli.py" && chmod +x /usr/bin/speedtest
-wget -q -O /usr/bin/bckpbot "https://raw.githubusercontent.com/darnix1/Premium/main/menu/bckpbot.sh" && chmod +x /usr/bin/bckpbot
-wget -q -O /usr/bin/tendang "https://raw.githubusercontent.com/darnix1/Premium/main/menu/tendang.sh" && chmod +x /usr/bin/tendang
-wget -q -O /usr/bin/bottelegram "https://raw.githubusercontent.com/darnix1/Premium/main/menu/bottelegram.sh" && chmod +x /usr/bin/bottelegram
-wget -q -O /usr/bin/m-allxray "https://raw.githubusercontent.com/darnix1/Premium/main/menu/m-allxray.sh" && chmod +x /usr/bin/m-allxray
-wget -q -O /usr/bin/xraylimit "https://raw.githubusercontent.com/darnix1/Premium/main/menu/xraylimit.sh" && chmod +x /usr/bin/xraylimit
-wget -q -O /usr/bin/trialvmess "https://raw.githubusercontent.com/darnix1/Premium/main/menu/trialvmess.sh" && chmod +x /usr/bin/trialvmess
-wget -q -O /usr/bin/trialvless "https://raw.githubusercontent.com/darnix1/Premium/main/menu/trialtrojan.sh" && chmod +x /usr/bin/trialtrojan
-wget -q -O /usr/bin/trialtrojan "https://raw.githubusercontent.com/darnix1/Premium/main/menu/trialvless.sh" && chmod +x /usr/bin/trialvless
-wget -q -O /usr/bin/trialssh "https://raw.githubusercontent.com/darnix1/Premium/main/menu/trialssh.sh" && chmod +x /usr/bin/trialssh
-#wget -q -O /usr/bin/autocpu "https://raw.githubusercontent.com/darnix1/Premium/main/install/autocpu.sh" && chmod +x /usr/bin/autocpu
-wget -q -O /usr/bin/bantwidth "https://raw.githubusercontent.com/darnix1/Premium/main/install/bantwidth" && chmod +x /usr/bin/bantwidth
-echo -e " [INFO] Download File Successfully"
-sleep 2
-exit
+
+# Hapus menu yang lama
+declare -a files=(
+    "backup" "bckpbot" "bottelegram" "install-up" "m-allxray" "m-backup" "m-bot"
+    "m-sshovpn" "m-ssws" "m-system" "m-tcp" "m-theme" "m-trojan" "m-update"
+    "m-vless" "m-vmess" "menu" "restore" "running" "tendang" "trial" "trialssh"
+    "trialtrojan" "trialvless" "trialvmess" "update" "xraylimit"
+)
+
+for file in "${files[@]}"; do
+    rm -rf "/usr/bin/$file"
+done
+
+fun_bar() {
+    CMD[0]="$1"
+    CMD[1]="$2"
+    (
+        [[ -e $HOME/fim ]] && rm $HOME/fim
+        ${CMD[0]} -y >/dev/null 2>&1
+        ${CMD[1]} -y >/dev/null 2>&1
+        touch $HOME/fim
+    ) >/dev/null 2>&1 &
+    tput civis
+    echo -ne "  \033[0;33mPlease Wait Loading \033[1;37m- \033[0;33m["
+    while true; do
+        for ((i = 0; i < 18; i++)); do
+            echo -ne "\033[0;32m#"
+            sleep 0.1s
+        done
+        [[ -e $HOME/fim ]] && rm $HOME/fim && break
+        echo -e "\033[0;33m]"
+        sleep 1s
+        tput cuu1
+        tput dl1
+        echo -ne "  \033[0;33mPlease Wait Loading \033[1;37m- \033[0;33m["
+    done
+    echo -e "\033[0;33m]\033[1;37m -\033[1;32m OK !\033[1;37m"
+    tput cnorm
+}
+
+res1() {
+    base_url="https://raw.githubusercontent.com/darnix1/mx2/main/menu"
+    declare -A downloads=(
+        ["backup"]="backup.sh" ["bckpbot"]="bckpbot.sh" ["bottelegram"]="bottelegram.sh"
+        ["install-up"]="../install-up.sh" ["m-allxray"]="m-allxray.sh" ["m-backup"]="m-backup.sh"
+        ["m-bot"]="m-bot.sh" ["m-sshovpn"]="m-sshovpn.sh" ["m-ssws"]="m-ssws.sh" ["m-system"]="m-system.sh"
+        ["m-tcp"]="m-tcp.sh" ["m-theme"]="m-theme.sh" ["m-trojan"]="m-trojan.sh" ["m-update"]="m-update.sh"
+        ["m-vless"]="m-vless.sh" ["m-vmess"]="m-vmess.sh" ["menu"]="menu.sh" ["restore"]="restore.sh"
+        ["running"]="running.sh" ["tendang"]="tendang.sh" ["trial"]="trial.sh" ["trialssh"]="trialssh.sh"
+        ["trialtrojan"]="trialtrojan.sh" ["trialvless"]="trialvless.sh" ["trialvmess"]="trialvmess.sh"
+        ["update"]="update.sh" ["xraylimit"]="xraylimit.sh"
+    )
+
+    for file in "${!downloads[@]}"; do
+        wget -q -O "/usr/bin/$file" "$base_url/${downloads[$file]}" && chmod +x "/usr/bin/$file"
+    done
+    
+    chmod +x /usr/bin/*
+    clear
+}
+
+echo -e "\n  \033[1;91m Update Script...\033[1;37m"
+fun_bar 'res1'
+
+echo -e ""
+menu
